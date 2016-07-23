df68ded4681c3822e53c6a60bc59218cc9b11dc6
David <kansas@inkwhiskey.com>
2016-07-23 01:31:35 -0600

    parallax theme thing and mobile sizes

--- _public/static/_js/main.js
+++ _public/static/_js/main.js
@@ -70,14 +70,39 @@
 
 // INTERACTIVE BACKGROUND
 
-var bg = document.querySelector('.theme-feature')
-bg.style.backgroundSize = "200%";
-var containerWidth = bg.offsetWidth
-var containerHeight = bg.offsetHeight
-window.addEventListener('mousemove', function(e) {
-    var amountX = (e.pageX / 500)
-    var amountY = (e.pageY / 250)
-    bg.style.backgroundPosition = amountX + '% ' + amountY + '%'
-})
+// http://www.onextrapixel.com/2014/04/09/create-an-interactive-moving-backgroundobject-that-reacts-to-viewers-cursor/
+// $(".bg").interactive_bg({
+//    strength: 25,
+//    scale: 1.05,
+//    animationSpeed: "100ms",
+//    contain: true,
+//    wrapContent: false
+//  });
+
+// http://codepen.io/chrisboon27/pen/rEDIC
+$(document).ready(function() {
+	var movementStrength = 25;
+	var height = movementStrength / $(window).height();
+	var width = movementStrength / $(window).width();
+	$(".theme-feature").mousemove(function(e){
+	          var pageX = e.pageX - ($(".theme-feature").width() / 2);
+	          var pageY = e.pageY - ($(".theme-feature").height() / 2);
+	          var newvalueX = width * pageX * -1;
+	          var newvalueY = height * pageY * -1;
+	          $('.theme-feature').css("background-position", newvalueX+"px "+newvalueY+"px");
+	});
+});
+
+// MAX
+
+// var bg = document.querySelector('.theme-feature')
+// bg.style.backgroundSize = "200%";
+// var containerWidth = bg.offsetWidth
+// var containerHeight = bg.offsetHeight
+// window.addEventListener('mousemove', function(e) {
+//     var amountX = (e.pageX / 500)
+//     var amountY = (e.pageY / 250)
+//     bg.style.backgroundPosition = amountX + '% ' + amountY + '%'
+// })
 
 
